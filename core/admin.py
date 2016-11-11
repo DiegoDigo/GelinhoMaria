b3ecd53eff75d7b42a049ee7887caad7e2d3f71a
Diego <di3g0d0ming05@gmail.com>
2016-11-11 09:25:28 -0200

    finalizacao do gelinhos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import TipoGelinho, SaborGelinho
 
-# Register your models here.
+
+class AdminTipoGelinho(admin.ModelAdmin):
+    list_display = ['tipo']
+    ordering = ['tipo']
+
+
+
+class AdminSaborGelinho(admin.ModelAdmin):
+    list_display = ['sabor','qtd','valor_uni','valor_total']
+    ordering = ['sabor']
+
+admin.site.register(TipoGelinho,AdminTipoGelinho)
+admin.site.register(SaborGelinho,AdminSaborGelinho)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 
+class TipoGelinho(models.Model):
+    tipo = models.CharField('Tipo', max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tipo
+
+    class Meta:
+        ordering = ['tipo']
+        verbose_name = 'TipoGelinho'
+        verbose_name_plural = 'TiposGelinho'
+
+
+class SaborGelinho(models.Model):
+    sabor = models.CharField('Sabor', max_length=100, unique=True)
+    tipo_gelinho = models.OneToOneField(TipoGelinho)
+    qtd = models.IntegerField('Quatidade')
+    valor_uni = models.DecimalField('Valor Unitario', max_digits=5, decimal_places=2)
+    valor_total = models.DecimalField('Valor Total', max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return self.sabor
+
+    class Meta:
+        ordering = ['sabor']
+        verbose_name ='SaborGelinho'
+        verbose_name_plural ='SaboresGelinhos'
+        default_related_name = 'Tipos'
